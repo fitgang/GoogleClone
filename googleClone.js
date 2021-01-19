fd33db339761000ccdfd55a808b6f9ef9aedda6b
fitgang <70760483+fitgang@users.noreply.github.com>
2021-01-19 18:33:55 +0530

    Create googleClone.js

---
+++ googleClone.js
@@ -0,0 +1,12 @@
+function lanCall(Lan) {
+    Lan = prompt("Which language can you write?", "Other than english and hindi");
+    if (Lan == null) {
+        alert("No problem. Go learn any one. Internet is for everyone.")
+    } else {
+        alert("Hmmmm...");
+    }
+}
+
+function luckFun() {
+    alert("That's great..");
+}
